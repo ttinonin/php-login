776737653f018e890f1a76b9d05e3d63d81e0d66
ttinonin <tinonin935@gmail.com>
2023-02-14 00:08:03 -0300

    Adding mysql database

--- cadastro.php
+++ cadastro.php
@@ -1,11 +1,11 @@
 <html>
     <body>
-        <h1>Daniel Menezes</h1>
+        <h1>Cadastro</h1>
         <form action="controle.php" method="post">
             <input type="text" name="nome">
             <input type="password" name="senha">
+            <input type="hidden" name="op" value="cad">
             <button type="submit">Enviar!</button>
         </form>
-        <a href="cadastro.php">Cadastro</a>
     </body>
 </html>
--- controle.php
+++ controle.php
@@ -1,12 +1,39 @@
 <?php
+    $connection = mysqli_connect("localhost", "root", "", "studentdb");
+
+    function handleLogin($connection, $nome, $senha) {
+        if(!$connection){
+            die("Could not connect".mysqli_connect_error());
+        } 
+        
+        $query = "SELECT * FROM students";
+        $stmt = mysqli_query($connection, $query);
+
+        while($row = mysqli_fetch_array($stmt, MYSQLI_ASSOC)){
+            if(($nome != $row['Nome']) && ($senha != $row['Senha'])){
+                header('Location: erro.php');
+            } else {
+                header('Location: home.php');
+            }
+        }
+    }
+
+    function handleCad($connection, $nome, $senha) {
+        $insertion = "INSERT INTO students (Nome, Senha) VALUES ('$nome', '$senha')";
+    
+        mysqli_query($connection, $insertion);
+
+        header('Location: index.php');
+    }
+
     $nome = $_POST["nome"];
     $senha = $_POST["senha"];
+    $op = $_POST['op'];
 
-    if($senha != "daniel"){
-        header('Location: erro.php');
+    if($op == 'cad'){
+        handleCad($connection, $nome, $senha);
     } else {
-        echo "<h1>Hello $nome your password is: $senha</h1>";
+        handleLogin($connection, $nome, $senha);
     }
-    
 ?>
 
